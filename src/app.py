759d0a5b19d2008dc28a4976de842ed454a38223
Bibek Acharya <71765138+bibekach@users.noreply.github.com>
2023-05-11 21:30:42 +0000

    added get all member and mender with id

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -16,26 +16,37 @@ CORS(app)
 jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
+    x = {
         "family": members
     }
+    return jsonify(x), 200
 
+@app.route('/members/<int:member_id>', methods=['GET'])
+def handle_he(member_id):
 
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.get_member(member_id)
+    response_body = {
+        "family": member
+    }
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
--- src/datastructures.py
+++ src/datastructures.py
@@ -8,12 +8,26 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {"id": self._generateId(),
+             "name": "John Jackson",
+             "age": 33,
+             "lucky_numbers": [7, 13, 22]},
+            {"id": self._generateId(),
+             "name": "John Jackson",
+             "age": 35,
+             "lucky_numbers": [10, 14, 3]},
+            {"id": self._generateId(),
+             "name": "Jimmy Jackson",
+             "age": 5,
+             "lucky_numbers": [7]}
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,16 +35,21 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        self._members.append(member)
+        return self._members
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for x in self._members:
+            if id == x["id"]:
+                self._members.pop(x)
+        return self._members
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for x in self._members:
+            if x["id"] == id:
+                return x
 
     # this method is done, it returns a list with all the family members
+
     def get_all_members(self):
         return self._members
